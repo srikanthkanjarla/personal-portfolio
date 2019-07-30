2915588a1163f9719221cc701cc8f05d85df2685
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-07-30 21:00:44 +0530

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Srikanth Kanjarla - Portfolio`,
+    description: `Front-End Developer, JavaScript, React.js`,
+    author: `@srikanthkanjarla`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve:`gatsby-plugin-typography`,
+      options:{
+        pathToConfigModule:`src/utils/typography`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -19,7 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `srikanth portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
