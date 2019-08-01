78868ecb0b70304e0b90e9be8aba821be5336983
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-01 12:17:13 +0530

    Add header with navbar component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,18 +7,18 @@ import './header.css';
 
 
 const Header = () => (
-    <header class="header" id="nav-bar">
-      <nav class="navbar">
-        <div class="logo">
-          <p class="logo-text"></p>
+    <header className="header" id="nav-bar">
+      <nav className="navbar">
+        <div className="logo">
+          <p className="logo-text">&lt; S &gt;</p>
         </div>
-        <div>
+        <div className="nav-links">
           <AnchorLink href="#home">Home</AnchorLink>
           <AnchorLink href="#portfolio">Portfolio</AnchorLink>
           <AnchorLink href="#about">About Me</AnchorLink>
           <AnchorLink href="#contact">Contact</AnchorLink>
-          <a href="https://github.com/srikanthkanjarla" target="_blank">
-            <FontAwesome name="github" size="2x" />
+          <a href="https://github.com/srikanthkanjarla" target="_blank" rel="noopener noreferrer" title="github profile">
+            Github
           </a>
         </div>
       </nav>
