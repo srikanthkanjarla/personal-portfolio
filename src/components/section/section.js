3bae470d4255cb018332e592ec25a9ba7f45e215
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-01 21:28:08 +0530

    Update section layout component

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './section.css';
 
 const Section = (props) =>{
+    const{ id,title } = props;
     return(
-        <section className="container">
+        <section className="container" id={id}>
+            <h3>{title}</h3>
             {props.children}
         </section>
     )
