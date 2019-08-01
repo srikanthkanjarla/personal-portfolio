dc14b1ef915ddeb80e051c9ae4b2ec31fcb89d0e
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-01 12:25:07 +0530

    Add contact me section

---
+++ src/sections/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionLayout from '../components/section/section';
+
+const Contact = () => {
+    return(
+        <SectionLayout>
+            <h1 id="contact">Contact me</h1>
+        </SectionLayout>
+    )
+}
+
+export default Contact;
