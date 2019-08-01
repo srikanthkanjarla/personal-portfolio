904464c11c5ad657970310baf74a771b56847cf1
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-01 21:25:16 +0530

    Add project card component

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,34 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
 
 const Layout = (props) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
   const {children} = props;
   return (
     <>       
-      <div>
+      <div id="home">
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+        <footer style={{textAlign:"center"}}>
+          © {new Date().getFullYear()}
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://github.com/srikanthkanjarla">srikanth kanjarla</a>
         </footer>
       </div>
     </>
