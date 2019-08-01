46ba6433dce78892c4f6dfba2f8b57aeb4aa2a1b
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-01 21:29:26 +0530

    update projects section component path

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/layout/layout";
 import SEO from "../components/seo";
 import Home from "../sections/home/home";
-import Projects from "../sections/projects";
+import Projects from "../sections/projects/projects";
 import About from "../sections/about";
 import Contact from "../sections/contact";
 
