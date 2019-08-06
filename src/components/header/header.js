077b7805600e0f6c4ddb5d8338dd3789f5207106
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-08-06 20:09:30 +0530

    Update github link in navbar
    added github fontawesome icon
    removed box-shadow, added border 2px

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,26 +1,35 @@
-// import { Link } from "gatsby"
 import React from 'react';
+
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-// import FontAwesome from "react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './header.css';
 
-const Header = () => (
-  <header className="header" id="nav-bar">
-    <nav className="navbar">
-      <div className="logo">
-        <p className="logo-text">&lt; S &gt;</p>
-      </div>
-      <div className="nav-links">
-        <AnchorLink href="#home">Home</AnchorLink>
-        <AnchorLink href="#portfolio">Portfolio</AnchorLink>
-        <AnchorLink href="#about">About Me</AnchorLink>
-        <AnchorLink href="#contact">Contact</AnchorLink>
-        <a href="https://github.com/srikanthkanjarla" target="_blank" rel="noopener noreferrer" title="github profile">
-          Github
-        </a>
-      </div>
-    </nav>
-  </header>
-);
+const Header = () => {
+  return (
+    <header className="header" id="nav-bar">
+      <nav className="navbar">
+        <div className="logo">
+          <p className="logo-text">&lt; S &gt;</p>
+        </div>
+        <div className="nav-links">
+          <AnchorLink href="#home">Home</AnchorLink>
+          <AnchorLink href="#portfolio">Portfolio</AnchorLink>
+          <AnchorLink href="#about">About Me</AnchorLink>
+          <AnchorLink href="#contact">Contact</AnchorLink>
+          <a
+            href="https://github.com/srikanthkanjarla"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="github profile"
+            className="github-profile"
+          >
+            <FontAwesomeIcon icon={faGithub} style={{ fontSize: '1.2em' }} /> Github
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
